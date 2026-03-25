dfc153e6ccae40c2963037eb82db4629371d2da3
brahmika <brahmika30@gmail.com>
2026-03-25 15:10:27 +0530

    UC2 - Added Service Layer to return Hello World greeting.

--- src/main/java/com/bridgelabz/GreetingApp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/GreetingApp/controller/GreetingController.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.GreetingApp.controller;
 
 import com.bridgelabz.GreetingApp.dto.GreetingDTO;
+import com.bridgelabz.GreetingApp.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,24 +13,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
-    //UC1 - GET
+
+    // Inject Service Layer
+    @Autowired
+    private GreetingService greetingService;
+
+    // UC1 - GET
     @GetMapping
-    public GreetingDTO getGreeting(){
-        return new GreetingDTO("Hello World - GET");
-    }
-    //UC1 - POST
-    @PostMapping
-    public GreetingDTO postGreeting(){
-        return new GreetingDTO("Hello World - POST");
-    }
-    //UC1 - PUT
-    @PutMapping
-    public GreetingDTO putGreeting(){
-        return new GreetingDTO("Hello World - PUT");
-    }
-    @DeleteMapping
-    public GreetingDTO deleteGreeting(){
-        return new GreetingDTO("Hello World - DELETE");
+    public GreetingDTO getGreeting() {
+        return new GreetingDTO(greetingService.getGreetingMessage());
     }
 
+    // UC1 - POST
+    @PostMapping
+    public GreetingDTO postGreeting() {
+        return new GreetingDTO(greetingService.getGreetingMessage());
+    }
+
+    // UC1 - PUT
+    @PutMapping
+    public GreetingDTO putGreeting() {
+        return new GreetingDTO(greetingService.getGreetingMessage());
+    }
+
+    // UC1 - DELETE
+    @DeleteMapping
+    public GreetingDTO deleteGreeting() {
+        return new GreetingDTO(greetingService.getGreetingMessage());
+    }
 }
---
+++ src/main/java/com/bridgelabz/GreetingApp/service/GreetingService.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.GreetingApp.service;
+import org.springframework.stereotype.Service;
+@Service
+public class GreetingService {
+    public String getGreetingMessage(){
+        return "Hello World";
+    }
+}
